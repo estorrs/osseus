f3a3a9c4b282471281155626e28ff4ad1115805a
Erik Storrs <estorrs@Eriks-MBP.attlocal.net>
2018-05-19 12:58:48 -0500

    listening on 8080

--- app/gunicorn_config.py
+++ app/gunicorn_config.py
@@ -1,5 +1,5 @@
 workers = 1
 worker_class = 'eventlet'
-bind = '0.0.0.0:443'
-certfile = 'ssl/server.crt'
-keyfile = 'ssl/server.key'
+bind = '0.0.0.0:8080'
+# certfile = 'ssl/server.crt'
+# keyfile = 'ssl/server.key'
